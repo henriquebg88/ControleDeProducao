d65b8cee25e9d920ce65313e5f274ff048e5619c
Henrique <henriquebg88@gmail.com>
2022-03-08 23:12:47 -0300

    Inicio do projeto. Que fofo!

---
+++ src/main/java/com/henrique/controleproducao/ControleproducaoApplication.java
@@ -0,0 +1,13 @@
+package com.henrique.controleproducao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleproducaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleproducaoApplication.class, args);
+	}
+
+}
